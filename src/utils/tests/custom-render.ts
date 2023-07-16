4e7583abfd0396d551038699ebcfc42ad25f8450
Matheus Ribeiro <srdmatheus@gmail.com>
2023-07-16 17:56:47 -0300

    config: add all configs

---
+++ src/utils/tests/custom-render.ts
@@ -0,0 +1,13 @@
+import { ReactElement } from "react";
+import { RenderOptions, render } from "@testing-library/react";
+
+interface customRenderProps {
+  ui: ReactElement;
+  options?: Omit<RenderOptions, "wrapper">;
+}
+
+const customRender = ({ ui, options }: customRenderProps) =>
+  render(ui, { ...options });
+
+export * from "@testing-library/react";
+export { customRender as render };
