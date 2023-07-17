0b3dcf403f0d2d73216165f7ad32da727502b9a3
Matheus Ribeiro <srdmatheus@gmail.com>
2023-07-17 13:01:22 -0300

    chore: clear file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,8 @@
-import Image from "next/image";
-
-import { allPosts } from "contentlayer/generated";
-
-import { Mdx } from "@/components/Mdx";
-
 export default function Home() {
-  const posts = allPosts.map((post) => post);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>Hello World!</h2>
-
-      <div dangerouslySetInnerHTML={{ __html: posts[0]?.title }} />
-      <Image src={posts[0].image} width={700} height={700} alt="" />
-      <Mdx code={posts[0].body.code} />
+      <div className="h-[2000px]">asd</div>
     </main>
   );
 }
