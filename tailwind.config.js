f2a97a765f2a26134b82fe3bf3ae5b73e63cd044
Matheus Ribeiro <srdmatheus@gmail.com>
2023-07-17 13:01:46 -0300

    chore: add extend colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      fontFamily: {
+        permanent: ["Permanent Marker", "cursive"],
+        foldit: ["Foldit", "cursive"]
+      },
+      colors: {
+        primary: "rgb(250, 250, 250)",
+        secondary: "#2e3038",
+        link: "#0683ff"
       }
     }
   },
