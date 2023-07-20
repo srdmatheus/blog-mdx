b27ffa21df86d4bf263a98694b0b15604d65b749
Matheus Ribeiro <srdmatheus@gmail.com>
2023-07-20 19:36:36 -0300

    refact: lint footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -17,7 +17,12 @@ export const Footer = ({ items }: FooterProps) => {
       <S.ListSociaLinks>
         {items.map((item) => (
           <S.ItemSocialLinks key={item.name}>
-            <Link href={item.href} title={item.name} className="block p-2">
+            <Link
+              href={item.href}
+              title={item.name}
+              className="block p-2"
+              target="_blank"
+            >
               {item.icon}
             </Link>
           </S.ItemSocialLinks>
