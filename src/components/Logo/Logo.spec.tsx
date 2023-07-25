9dd0e04095da268deae1541a63fe659102ad6a03
Matheus Ribeiro <srdmatheus@gmail.com>
2023-07-25 12:09:11 -0300

    refact: change logo's structure files

--- src/components/Logo/Logo.spec.tsx
+++ src/components/Logo/Logo.spec.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 
-import { Logo } from "./Logo";
+import { Logo } from "./index";
 
 describe("Logo", () => {
   it("should render successfully", () => {
