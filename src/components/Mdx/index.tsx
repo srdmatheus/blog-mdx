12b820a67d911b55a8aa24520c467478db4a077a
Matheus Ribeiro <srdmatheus@gmail.com>
2023-07-27 10:28:32 -0300

    feat: add new tags to mdx

--- src/components/Mdx/index.tsx
+++ src/components/Mdx/index.tsx
@@ -6,9 +6,50 @@ import "@/styles/mdx.css";
 
 const components: MDXComponents = {
   h1: ({ className = "", children, ...props }) => (
-    <h1 className={`text-4xl font-bold ${className}`} {...props}>
+    <h1
+      className={`text-3xl font-black ${className} text-neutral-900`}
+      {...props}
+    >
       {children}
     </h1>
+  ),
+  h2: ({ className = "", children, ...props }) => (
+    <h2 className={`text-2xl font-black ${className}`} {...props}>
+      {children}
+    </h2>
+  ),
+  h3: ({ className = "", children, ...props }) => (
+    <h3 className={`text-xl font-black ${className}`} {...props}>
+      {children}
+    </h3>
+  ),
+  h4: ({ className = "", children, ...props }) => (
+    <h4 className={`text-lg font-black ${className}`} {...props}>
+      {children}
+    </h4>
+  ),
+  h5: ({ className = "", children, ...props }) => (
+    <h5 className={`text-base font-black ${className}`} {...props}>
+      {children}
+    </h5>
+  ),
+  h6: ({ className = "", children, ...props }) => (
+    <h6 className={`text-sm font-black ${className}`} {...props}>
+      {children}
+    </h6>
+  ),
+  a: ({ className = "", children, ...props }) => (
+    <a
+      className={`hover:brightness-80 text-link transition-colors duration-300 ${className}`}
+      {...props}
+    >
+      {children}
+    </a>
+  ),
+  p: ({ className = "", children, ...props }) => (
+    <p className={`mb-4 text-lg leading-relaxed ${className}`} {...props}>
+      {children}
+    </p>
   ),
   pre: ({ className = "", children, ...props }) => (
     <pre
