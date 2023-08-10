c9e7486b5c7bf8ec155d5913c14061718727db43
Matheus Ribeiro <srdmatheus@gmail.com>
2023-08-10 12:48:39 -0300

    feat: add paginationPages

--- src/utils/functions/index.ts
+++ src/utils/functions/index.ts
@@ -2,3 +2,4 @@ export * from "./slugfy";
 export * from "./format-date";
 export * from "./format-posts";
 export * from "./pagination-posts";
+export * from "./pagination-pages";
---
+++ src/utils/functions/pagination-pages/index.ts
@@ -0,0 +1,7 @@
+export const paginationPages = (currentPage = 1, pageRoute = "/page") => {
+  const prevPage =
+    currentPage - 1 === 1 ? "/" : `${pageRoute}/${currentPage - 1}`;
+  const nextPage = `${pageRoute}/${currentPage + 1}`;
+
+  return { prevPage, nextPage };
+};
