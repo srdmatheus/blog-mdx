1b100f2b2409b32c9645f6d3261b1fe431355d5f
Matheus Ribeiro <srdmatheus@gmail.com>
2023-08-10 12:48:16 -0300

    refact: optimize function

--- src/utils/functions/pagination-posts/index.ts
+++ src/utils/functions/pagination-posts/index.ts
@@ -1,5 +1,9 @@
 import { BlogPost } from "@/types";
 
 export const paginationPosts = (posts: BlogPost[], limit = 10, page = 1) => {
-  return { posts, limit, page };
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const totalPosts = posts.slice(startIndex, endIndex);
+
+  return totalPosts;
 };
